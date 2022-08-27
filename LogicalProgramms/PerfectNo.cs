91888093b5bbc729b08943867412d1259ed20c4f
sayalipagar <pagarsayali565@gmail.com>
2022-08-27 13:50:38 +0530

    [sayali]Add:PerfectNo

---
+++ LogicalProgramms/PerfectNo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgramms
+{
+    public class PerfectNo
+    {
+        int numCheck = 0;
+        public void Perfect()
+        {
+            Console.WriteLine("Enter the no to check Either its perfect no or not");
+            int num =Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= num / 2; i++)
+            {
+                if (num % 1 == 0)
+                {
+                    numCheck += i;
+
+                }
+            }
+            if (numCheck == num)
+            {
+                Console.WriteLine(" Entered number is Perfect no");
+            }
+            else
+            {
+                Console.WriteLine("\n Entered number is not a perfect number");
+                Console.ReadLine();
+            }
+        }
+    }
+}
--- LogicalProgramms/Program.cs
+++ LogicalProgramms/Program.cs
@@ -6,13 +6,17 @@
         {
             while (true)
             {
-                Console.WriteLine("select Program\n 1.FibonacciSeries");
+                Console.WriteLine("\nselect Program\n 1.FibonacciSeries\n 2.Perfect No");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
                     case 1:
                         FibonacciSeries series = new FibonacciSeries();
                         series.print();
+                        break;
+                    case 2:
+                        PerfectNo perfectNo = new PerfectNo();
+                        perfectNo.Perfect();
                         break;
                 }
             }
